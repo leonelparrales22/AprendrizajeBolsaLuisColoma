a5df59af41715ec81fc935f25260b4f736c32d11
Leonel Parrales M <parralesdl@produbanco.com>
2025-12-13 23:18:22 -0500

    feat: add Tailwind CSS integration and update configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/app/page.tsx', './src/app/layout.tsx'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {},
   },
